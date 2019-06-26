682e08072da9930b8730f15305add6bcdaa91a93
Bola <bolanaguib@gmail.com>
2019-06-26 10:42:36 +0200

    [ modified ] products table

--- database/migrations/2019_06_26_083723_create_products_table.php
+++ database/migrations/2019_06_26_083723_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('details')->nullable();
+            $table->integer('price');
+            $table->text('description');
             $table->timestamps();
         });
     }

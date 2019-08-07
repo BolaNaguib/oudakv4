f3aa750659a05185e803cc22dbd0132d8798456d
Bola Naguib <bolanaguib@gmail.com>
2019-08-08 00:09:40 +0200

    [updated] app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // set laravel mail config from voyager settings
+        config([
+          'mail.host'         => settings('mailing-settings.host'),
+          'mail.port'         => settings('mailing-settings.port'),
+          'mail.from.address' => settings('mailing-settings.sender-email'),
+          'mail.from.name'    => settings('mailing-settings.sender-name'),
+          'mail.encryption'   => settings('mailing-settings.encryption') ?: null,
+          'mail.username'     => settings('mailing-settings.username'),
+          'mail.password'     => settings('mailing-settings.password'),
+        ]);
     }
 }

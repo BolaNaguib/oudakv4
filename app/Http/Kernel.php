ab025cc1bf51c8e32f74ea6817538a004b05b8e7
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2019-08-11 10:56:43 +0200

    add visits model and table

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLanguage::class,
-
+            \App\Http\Middleware\RegisterVisit::class,
         ],
 
         'api' => [

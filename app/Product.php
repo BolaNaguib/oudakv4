75754cdfab34a1ebf7c6e2ddffdadd3e5757ba5b
Bola <bolanaguib@gmail.com>
2019-08-20 23:01:28 +0200

    updated producrt

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\ZCRM\Modules\Product as ZCRMProduct;
 use App\ZCRM\Traits\MapsToZCRMModule;
+use App\ProductCategory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -18,18 +19,13 @@ class Product extends Model
     //   return money_format('$%i', $this->price / 100);
     // }
 
-    public function category(){
-        return $this->belongsTo(ProductCategory::class, 'product_category_id');
-    }
-
     public function mapToZCRMModule (ZCRMProduct $product) {
       //  question here are those fields from db ?  name ? i have it as a title not name
         $product->Product_Name = $this->title;
         $product->Unit_Price = $this->price;
         $product->Qty_in_Stock = $this->quantity;
         $product->Description = $this->main_description;
-        $cat = $this->category()->first();
-        if ($cat) $product->Product_Category = $cat->title;
+        $product->Product_Category = $this->belongsTo(ProductCategory::class, 'category')->value('title');
         $product->Product_Code = $this->serial_number;
     }
 }

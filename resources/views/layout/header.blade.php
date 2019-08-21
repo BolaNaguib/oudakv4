5eecaf08abaa64a53fb7d7d0c37a2375e30da86c
Bola <bolanaguib@gmail.com>
2019-08-21 17:34:48 +0200

    [ fixed ] view port

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,6 +4,7 @@
   <div class="preloader"></div>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="robots" content="nofollow" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title></title>

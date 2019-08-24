28684312f9e5eeb4763f126dc48c2ec85c0c02dc
Bola <bolanaguib@gmail.com>
2019-08-24 16:51:30 +0200

    [ fixed ] product_category_id

--- resources/views/product-portrait.blade.php
+++ resources/views/product-portrait.blade.php
@@ -316,7 +316,7 @@
         <div class="uk-child-width-1-4@m uk-child-width-1-2@s uk-child-width-1-1" uk-grid>
 
             @foreach ($products as $sproduct)
-            @if ($product->category == $sproduct->category)
+            @if ($product->product_category_id == $sproduct->product_category_id)
             @if ($product->slug != $sproduct->slug)
             <!-- START div -->
             <div class="">

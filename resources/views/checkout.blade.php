5ddfee9d1ffcbf5fab56679cc7d32cf1a30bc6f2
Bola <bolanaguib@gmail.com>
2019-08-26 03:27:55 +0200

    [ fixed ] checkout page mobile version

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -9,125 +9,13 @@
     <hr class="uk-margin-remove">
   </div>
 </section>
-<!-- START section -->
-{{-- <section class="uk-section">
-  <div class="uk-container uk-container-large">
-    <div class="uk-child-width-1-2" uk-grid>
-      <div class="">
-        <form class="uk-form-horizontal" action="index.html" method="post">
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Email / Username</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="text" placeholder="Enter Your Email Or Username">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Password </label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="password" placeholder="ENter Your Password">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-          <div class="uk-text-right">
-            <button class="uk-button uk-button-secondary" type="button" name="button"> Login </button>
-            <button class="uk-button uk-button-default" type="button" name="button"> Forgot Your Password ? </button>
-
-          </div>
-
-        </form>
-      </div>
-
-      <div class="">
-        <form class="uk-form-horizontal" action="index.html" method="post">
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">First Name</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="text" placeholder="First Name">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Last Name</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="text" placeholder="Last Name">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Address</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="text" placeholder="Address">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Phone Number</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand " id="form-stacked-text" type="number" placeholder="+1 01023 45687 330">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Email Address</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="email" placeholder="bola@naguib.com">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Password</label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input uk-width-expand" id="form-stacked-text" type="password" placeholder="Enter A password ">
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-
-          <!-- START .uk-margin -->
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Repeat Password </label>
-            <!-- START .uk-form-controls -->
-            <div class="uk-form-controls">
-              <input class="input error uk-width-expand" id="form-stacked-text" type="password" placeholder="Re-enter your password ">
-              <small class="uk-text-danger uk-text-small"> This Password Doesnt match </small>
-
-            </div><!-- END .uk-form-controls -->
-          </div><!-- END .uk-margin -->
-          <div class="uk-text-right">
-            <button class="uk-button uk-button-secondary" type="button" name="button"> Register </button>
-
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- END section --> --}}
-
-
 
 <!-- START section -->
 <section class="uk-section ">
   <!-- START .uk-container -->
   <div class="uk-container uk-container-large">
     <!-- START uk-grid -->
-    <div class="uk-child-width-1-2" uk-grid>
+    <div class="uk-child-width-1-2@m uk-width-1-1" uk-grid>
       <!-- START div -->
       <div class="">
         <h3>Billing / Shipping Info</h3>

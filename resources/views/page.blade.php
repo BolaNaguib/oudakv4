705e63f1acda9d7cec032280c55b134ff73095fa
Bola Naguib <bolanaguib@gmail.com>
2019-09-20 13:09:16 +0200

    fixed error when clicking on a page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,17 +1,15 @@
-@include('layout.header')
+@extends('layouts.app')
+@section('content')
 
-<section class="uk-section">
-  <div class="uk-container uk-container-large">
-    <img width="100%" src="{{ asset('storage/'.$page->image) }}" alt="">
-    {{-- <h1>{{ $page->getTranslatedAttribute('title', app()->getLocale(), 'en') }}</h1> --}}
-    <h1>{{ $page->title }}</h1>
-    <hr>
-    <div class="">
-      {!! $page->body !!}
+  <section class="uk-section">
+    <div class="uk-container uk-container-large">
+      <img width="100%" src="{{ asset('storage/'.$page->image) }}" alt="">
+      {{-- <h1>{{ $page->getTranslatedAttribute('title', app()->getLocale(), 'en') }}</h1> --}}
+      <h1>{{ $page->title }}</h1>
+      <hr>
+      <div class="">
+        {!! $page->body !!}
+      </div>
     </div>
-  </div>
-</section>
-
-
-
-@include('layout.footer')
+  </section>
+@endsection

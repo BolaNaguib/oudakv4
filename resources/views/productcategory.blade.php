11dc8d45392b87fec1f87253ff1d063004bbceca
Bola Naguib <bolanaguib@gmail.com>
2019-10-30 20:39:45 +0200

    [ updated grid view for product category ]

--- resources/views/productcategory.blade.php
+++ resources/views/productcategory.blade.php
@@ -14,15 +14,75 @@
     @endforeach
     @endif
 @if ($productcategory->parent == null)
+  <div class="uk-container uk-container-large">
+    <br>
+    <div class="uk-text-right">
+      <span> Diplay Mode : </span>
+      <button class="productlistx" type="button" name="gridoptionicon"><span uk-icon="icon: grid;"></span></button>
+      <button  class="productlistxv" type="button" name="gridoptioniconv"><span uk-icon="icon: more-vertical;"></span></button>
+    </div>
+    <hr>
+  </div>
+
 @foreach ($allcat as $cat )
 @if ($cat->parent == $productcategory->id)
 
-<div class="uk-container uk-container-large uk-margin-large">
+<div class="categorylist uk-container uk-container-large uk-margin-large">
     <a href="/category/{{ $cat->slug }}">
         <div class="uk-background-center-center uk-height-large uk-width-expand parralexbg" style="background-image: url({{ asset('storage/'.$cat->path) }});">
             <p class="uk-h4 uk-margin-remove uk-light newcattit">{{ $cat->title }}</p>
         </div>
     </a>
+
+</div>
+<div class=" productlist uk-container uk-container-large uk-margin-large">
+  <h3>{{ $cat->title }}</h3>
+  <hr>
+  <div class="uk-grid uk-grid-match">
+    @foreach ($products as $product )
+    @if ($product->category == $cat->id)
+      <!-- START .uk-width-1-3@m -->
+    <div class="uk-width-1-3@m uk-width-1-2 ">
+      <!-- START div -->
+      <div class="card card_theme_white">
+        <form class="" action="{{ route('cart.store') }}" method="post">
+            {{ csrf_field() }}
+            <div class="">
+
+              <div class="uk-text-center">
+                <b>{{ $product->title }}</b>
+              </div>
+            <!-- START .card -->
+            <div class=" uk-flex uk-flex-middle uk-flex-center uk-position-relative uk-transition-toggle" tabindex="0">
+
+            <a href="{{ route('shop.show', $product->slug)}}">
+                <!-- START .uk-inline-clip -->
+                <div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+                    <img src="{{ asset('storage/'.$product->thumbnail) }}" alt="" style="max-height:350px;">
+                    <img class="uk-transition-fade uk-position-cover" src="{{ asset('storage/'.$product->secondimage) }}" alt="" style="max-height:250px;">
+                </div><!-- END .uk-inline-clip -->
+            </a>
+            <input type="hidden" name="id" value="{{ $product->id }}">
+            <input type="hidden" name="name" value="{{ $product->title }}">
+            <input type="hidden" name="price" value="{{ $product->price }}">
+            </div><!-- END .card -->
+
+
+            </div><!-- END .card_theme_white -->
+            <div class="uk-text-center">
+            <span> ${{ $product->price }} </span>
+            <hr>
+
+            </div>
+            <div class="uk-text-center">
+              <button class="button_type_category_product" type="submit" name="button"> <span uk-icon="cart"></span> <span style="line-height: 1.8;">ADD</span>  </button>
+            </div>
+            </form>
+            </div>
+    </div>
+    @endif
+    @endforeach
+  </div>
 
 </div>
 
@@ -32,6 +92,7 @@
 <!-- START .uk-section -->
 <section class="uk-section" style="padding-top:20px;">
 <div class="uk-container uk-container-large">
+  <hr>
   <div class="uk-text-center">
     <h3> {{ $productcategory->title }} </h3>
 
@@ -340,6 +401,20 @@ background-color: transparent;
 border: none;
 cursor: pointer;
 }
+.productlist{
+  display: none;
+}
+.productlistx{
+background-color: transparent;
+border: none;
+cursor: pointer;
+}
+.productlistxv{
+background-color: transparent;
+border: none;
+cursor: pointer;
+}
+
 </style>
 
 @endsection
@@ -358,5 +433,24 @@ cursor: pointer;
       $gridoption.removeClass('uk-width-1-1');
       $gridoption.addClass('uk-width-1-4@m uk-width-1-2');
     });
+
+
+
+
+    let $productlistx = $('.productlistx');
+    let $productlistxv = $('.productlistxv');
+    let $productlist = $('.productlist');
+    let $categorylist = $('.categorylist');
+    console.log("HEllo");
+      $productlistxv.on('click',function(){
+        $productlist.hide();
+        $categorylist.show();
+
+      });
+      $productlistx.on('click',function(){
+
+        $productlist.show();
+        $categorylist.hide();
+      });
   </script>
 @endsection

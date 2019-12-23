963e5f7938b9283b3b1bb4b5dc408871631f2911
Bola Naguib <bolanaguib@gmail.com>
2019-12-24 01:11:57 +0200

    [ Added ] Browser Validation

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -375,6 +375,17 @@ truepush.push(function(){
     console.log(giftname);
   });
 </script>
+<script>
+
+var ua = navigator.userAgent.toLowerCase(); 
+if (ua.indexOf('safari') != -1) { 
+  if (ua.indexOf('chrome') > -1) {
+    console.log("chrome");
+  } else {
+    console.log("safari");
+  }
+}
+</script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvPkBdWsK81JT4HptA_EZUSP6O9XfyZMs&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
 <script src="https://vjs.zencdn.net/7.5.5/video.js"></script>
 
